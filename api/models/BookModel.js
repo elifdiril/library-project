185983ef4c72481a5256002b40e5468fde5d3c2c
Elif <elifdiril6@gmail.com>
2023-09-22 17:38:53 +0300

    Create BookModel.js

---
+++ api/models/BookModel.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: true
+    },
+    comments: {
+        type: String,
+        required: false
+    }
+}, {timestamps: true});
+
+module.exports = mongoose.model('BookStore', bookSchema);
